0656de73299d7dee75aae6965b6d560a065cd38c
Chyngyz <mirlanuchyngyz@gmail.com>
2023-07-05 21:20:32 +0600

    pagination done

--- excoin/settings.py
+++ excoin/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 18,
+    'PAGE_SIZE': 20,
 }
 
 # Default primary key field type
